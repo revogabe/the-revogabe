aff13cc6a056b5a4c972f28cf47e36e5de61d04d
Daniel Gabriel <revogabe.dev@gmail.com>
2024-01-15 20:40:19 -0300

    Refactor error and loading components

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,9 +1,19 @@
-const Error = () => {
+'use client'
+
+// Error components must be Client Components
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
   return (
-    <main className="center flex h-screen w-full">
-      <h5>404 not found</h5>
+    <main className="center flex h-screen w-full flex-col gap-4 text-center">
+      <h2>Something went wrong! </h2>
+      <p className="max-w-[500px]">{error.message}</p>
     </main>
   )
 }
-
-export default Error
--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,9 @@
 import { Skeleton } from '@/ui/skeleton'
 
-const Loading = () => {
+export default function Loading() {
   return (
     <main className="center flex h-screen w-full">
       <Skeleton className="h-24 w-[140px]" />
     </main>
   )
 }
-
-export default Loading
