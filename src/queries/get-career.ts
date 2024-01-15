554c7c259e612753604c18821521304c8aa27455
Daniel Gabriel <revogabe.dev@gmail.com>
2024-01-15 18:28:52 -0300

    Remove cache option from API requests

--- src/queries/get-career.ts
+++ src/queries/get-career.ts
@@ -3,7 +3,6 @@ import { Career, CareerResponse } from '@/app/api/careers/route'
 export async function getCareers() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/careers`, {
     method: 'GET',
-    cache: 'force-cache',
     headers: {
       'Content-Type': 'application/json',
     },
--- src/queries/get-projects.ts
+++ src/queries/get-projects.ts
@@ -3,7 +3,6 @@ import { Project } from '@/app/api/projects/route'
 export async function getProjects() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/projects`, {
     method: 'GET',
-    cache: 'force-cache',
     headers: {
       'Content-Type': 'application/json',
     },
