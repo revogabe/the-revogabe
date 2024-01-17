0fda5290454595958401b558c480f1eeea638aa2
Daniel Gabriel <56942108+revogabe@users.noreply.github.com>
2024-01-17 00:44:09 -0300

    Update route.ts

--- src/app/api/roadmap/route.ts
+++ src/app/api/roadmap/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
       id: 'rust',
       title: 'Estudar Rust',
       description:
-        'Recentemente eu tenho estudado Rust ocasionalmente, e estou gostando bastante da linguagem. Acredito que ela tem um potencial enorme, e que pode ser uma das linguagens mais usadas no uturo. Por isso, estou estudando ela para me preparar para o futuro e para o que está por vir.',
+        'Recentemente eu tenho estudado Rust ocasionalmente, e estou gostando bastante da linguagem. Acredito que ela tem um potencial enorme, e que pode ser uma das linguagens mais usadas no futuro. Por isso, estou estudando ela para me preparar para o futuro e para o que está por vir.',
       badges: ['Rust', 'Study'],
     },
     {
